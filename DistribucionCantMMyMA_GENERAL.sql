5554b8edbaff48fc6e7d8f977178e6627a86b3ba
Hugo Montes <hmontes@cofar.com.bo>
2018-05-29 12:24:00 -0400

    Se ha mejorado la consulta para distribuciones

--- DistribucionCantMMyMA_GENERAL.sql
+++ DistribucionCantMMyMA_GENERAL.sql
@@ -8,7 +8,7 @@ INNER JOIN funcionarios f on f.codigo_funcionario=d.cod_visitador
 INNER JOIN ciudades c on c.cod_ciudad=f.cod_ciudad
 INNER JOIN lineas l on l.codigo_linea=d.codigo_linea
 where d.codigo_gestion='1015' and d.cod_ciclo='3'
-group by d.cod_visitador, d.codigo_producto, d.grupo_salida
+group by d.cod_visitador, d.codigo_producto, d.grupo_salida, l.nombre_linea
 UNION
 select 'MA' as tipo,c.descripcion as regional, f.cod_zeus, d.cod_visitador, CONCAT(f.paterno,' ',f.materno,' ',f.nombres) as nombre_visitador, d.codigo_linea, l.nombre_linea
 , d.codigo_producto, ma.descripcion_material as nombre_producto, sum(d.cantidad_planificada) as cantidad_planificada, SUM(d.cantidad_sacadaalmacen) as cantidad_sacalmacen
@@ -18,4 +18,5 @@ inner JOIN funcionarios f on f.codigo_funcionario=d.cod_visitador
 INNER JOIN ciudades c on c.cod_ciudad=f.cod_ciudad
 INNER JOIN lineas l on l.codigo_linea=d.codigo_linea
 where d.codigo_gestion='1015' and d.cod_ciclo='3'
-group by d.cod_visitador, d.codigo_producto, d.grupo_salida
+group by d.cod_visitador, d.codigo_producto, d.grupo_salida, l.nombre_linea
+
